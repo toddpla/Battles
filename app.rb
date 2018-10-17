90811fef99cf963bd5be9cb950f5877c557567c9
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-17 16:42:40 +0100

    Checks player score

--- app.rb
+++ app.rb
@@ -10,15 +10,21 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    session[:player_1] = { name: params[:player_1_name], score: 100 }
+    session[:player_2] = { name: params[:player_2_name], score: 100 }
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    session[:current_player] ||= :player_1
+    @player_1_name = session[:player_1][:name]
+    @player_2_name = session[:player_2][:name]
+    @opponent_score = player_score(session[:player_2])
+    session[:current_player] == :player_1 ? session[:current_player] == :player_2 : session[:current_player] == :player_1
     erb :play
   end
 
+  def player_score(player)
+    "#{player[:name]} : #{player[:score]}"
+  end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,3 +8,14 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
+
+feature 'Hit points' do
+  scenario 'player 1 sees player 2 hit points' do
+  visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
+  expect(page).to have_content 'Mittens : 100'
+end
+
+end
