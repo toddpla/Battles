6ef2e576b4ef73eca39cecad2dc6f40476dd5909
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-18 16:22:00 +0100

    refactored controller with Game class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
-require 'game'
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -10,24 +10,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(name: params[:player_1_name])
-    $player_2 = Player.new(name: params[:player_2_name])
-    $game = Game.new(players: [$player_1, $player_2])
+    player_1 = Player.new(name: params[:player_1_name])
+    player_2 = Player.new(name: params[:player_2_name])
+    $game = Game.new(player_1: player_1, player_2: player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_1_hp = $player_1.hp
-    @player_2_name = $player_2.name
-    @player_2_hp = $player_2.hp
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    $game.attack($player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 class Game
+  attr_reader :player_1, :player_2
 
-  def initialize(players:)
-    @players = players
+  def initialize(player_1:, player_2:)
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
   def attack(player)
     player.reduce_hp
   end
-    
+
 end
